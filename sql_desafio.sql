53b6a7262b5fde662f162df075beda4c40c519f3
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-06-24 14:23:31 -0300

    subindo desafios nivel intermediario feitos

--- sql_desafio.sql
+++ sql_desafio.sql
@@ -26,7 +26,6 @@ SELECT * FROM projetos WHERE data_inicio > '2023-01-01';
 /* Ordene os funcionários por idade em ordem crescente. */
 SELECT * FROM funcionarios ORDER BY idade ASC;
 
-
 /* Selecione os funcionários cujo nome começa com 'A'. */
 SELECT * FROM funcionarios WHERE nome LIKE 'a%';
 
@@ -40,16 +39,81 @@ SELECT * FROM projetos WHERE data_inicio < '2023-01-01';
 SELECT COUNT(nome) FROM funcionarios WHERE idade > 35;
 
 /* Intermediário */
+
 /* Liste os funcionários com seus respectivos projetos atribuídos. */
+SELECT funcionarios.nome AS NomeFuncionario, projetos.nome AS NomeProjeto, atribuicoes.papel
+FROM atribuicoes
+JOIN funcionarios ON atribuicoes.id_funcionario = funcionarios.id
+JOIN projetos ON atribuicoes.id_projeto = projetos.id;
+
+
 /* Liste os projetos com a contagem de funcionários atribuídos a cada um. */
+SELECT projetos.nome AS NomeProjeto, COUNT(atribuicoes.id_funcionario) AS ContagemFuncionarios
+FROM projetos
+LEFT JOIN atribuicoes ON projetos.id = atribuicoes.id_projeto
+GROUP BY projetos.nome;
+
+
+
 /* Selecione os funcionários que estão atribuídos ao projeto 'Projeto A'. */
+SELECT funcionarios.nome AS NomeFuncionarios
+FROM atribuicoes
+JOIN funcionarios ON atribuicoes.id_funcionario = funcionarios.id
+JOIN projetos ON atribuicoes.id_projeto = projetos.id
+WHERE projetos.nome = 'Projeto A';
+
+
+
 /* Liste os funcionários ordenados pelo salário em ordem decrescente. */
+SELECT *
+FROM funcionarios
+ORDER BY salario DESC;
+
+
+
 /* Selecione os projetos com descrição que contenha a palavra 'banco'. */
+SELECT *
+FROM projetos
+WHERE descricao LIKE '%banco%';
+
+
+
 /* Liste os funcionários e seus salários, arredondados para duas casas decimais. */
+SELECT nome, ROUND(salario, 2) AS SalarioArredondado
+FROM funcionarios;
+
+
 /* Selecione os projetos que terminam antes de '2023-07-01'. */
+SELECT *
+FROM projetos
+WHERE data_fim < '2023-07-01';
+
+
+
 /* Liste os funcionários com idade entre 25 e 30 anos. */
+SELECT * 
+FROM funcionarios
+WHERE idade BETWEEN 25 AND 30;
+
+
+
 /* Selecione os projetos onde nenhum funcionário foi atribuído. */
+SELECT projetos.*
+FROM projetos
+LEFT JOIN atribuicoes ON projetos.id = atribuicoes.id_projeto
+WHERE atribuicoes.id_projeto IS NULL;
+
+
+
 /* Liste os funcionários que não estão atribuídos a nenhum projeto. */
+SELECT funcionarios.*
+FROM funcionarios
+LEFT JOIN atribuicoes ON funcionarios.id = atribuicoes.id_funcionario
+WHERE atribuicoes.id_funcionario IS NULL;
+
+
+
+
 
 /* Avançado */
 /* Selecione os funcionários que estão atribuídos a mais de um projeto. */
